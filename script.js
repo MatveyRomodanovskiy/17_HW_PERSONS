33619900f01d562a9f73affd7f269edb0785dc89
Matvey Romodanovskiy <startpolus@gmail.com>
2023-03-15 22:02:03 +0200

    upgrade to 19HW

--- script.js
+++ script.js
@@ -3,58 +3,58 @@ document.getElementById("calcStats").disabled = true;
 const statsList = document.createElement('ul');
 statsTitle.after(statsList);
 addPerson.onclick = () => {
-    const person ={ID: '', fstName:'', lstName:'', age:0};
+    const person = {ID: '', fstName: '', lstName: '', age: 0};
     person.ID = personId.value.trim();
     person.fstName = firstName.value.trim();
     person.lstName = lastName.value.trim();
-    person.age = +age.value;
+    person.age = ((new Date().getTime() - new Date(age.value)) / (24 * 3600 * 365.25 * 1000)) | 0;
     addPersonToArrayandList(person);
 }
 
 calcStats.onclick = () => {
     statsList.innerHTML = '';
     const stats = [...persons];
-    stats.sort((p1 , p2) => p1.age - p2.age);
+    stats.sort((p1, p2) => p1.age - p2.age);
     statsList.appendChild(buildElem(stats[0], 'MIN age person: '));
-    statsList.appendChild(buildElem(stats[stats.length-1], 'MAX age person: '))
-    const averageAge = ((stats.reduce((acc, p) => acc + p.age, 0))/stats.length).toFixed(2);
+    statsList.appendChild(buildElem(stats[stats.length - 1], 'MAX age person: '))
+    const averageAge = ((stats.reduce((acc, p) => acc + p.age, 0)) / stats.length).toFixed(2);
     li = document.createElement('li');
     li.append(`Average age: ${averageAge} year(s)`);
     statsList.appendChild(li);
 }
-    function  addPersonToArrayandList(person) {
-    if (((persons.findIndex(p => p.ID === person.ID)) >= 0)){
-        console.log(persons.findIndex(p => p.ID === person.ID));
+
+function addPersonToArrayandList(person) {
+    if (((persons.findIndex(p => p.ID === person.ID)) >= 0)) {
         return false;
     }
-    document.getElementById("calcStats").disabled = false;
     persons.push(person);
-    const li = document.createElement('li');
-    li.id = person.ID;
-    const text = `ID: ${person.ID}, First name: ${person.fstName},  Last name: ${person.lstName}, 
-    age: ${person.age}`;
-    li.append(document.createTextNode(text), createButtonDel());
-    personsList.appendChild(li);
-    statsList.innerHTML = '';
+    if (persons.length > 0) {
+        document.getElementById("calcStats").disabled = false;
     }
-    function removePersone(ID) {
-        const index = persons.findIndex(p => ID === p.ID.toString());
+    const li =  createInfoElement(`ID: ${person.ID}, First name: ${person.fstName},  Last name: ${person.lstName}, 
+    age: ${person.age}`, 'li')
+    const btnDel = createButtonDel(() => {
+        statsList.innerHTML = '';
+        const index = persons.findIndex(({id}) => id === person.id);
         persons.splice(index, 1);
-        if(persons.length === 0){
+        if (persons.length === 0) {
             document.getElementById("calcStats").disabled = true;
         }
+    });
+    li.append(btnDel);
+    personsList.appendChild(li);
+    if(statsList.innerHTML){
         statsList.innerHTML = '';
     }
-
-function buildElem(stat, str) {
-    const minAgePerson = `${str} ID: ${stat.ID}, First name: ${stat.fstName},  Last name: ${stat.lstName},
-    age: ${stat.age}`;
-    const li = document.createElement('li');
-    li.append(minAgePerson);
-    return li;
 }
 
-
+function buildElem(stat, str) {
+    const minAndMaxAgePerson = `${str} ID: ${stat.ID}, First name: ${stat.fstName},  Last name: ${stat.lstName},
+    age: ${stat.age}`;
+    // const li = document.createElement('li');
+    // li.append(minAndMaxAgePerson);
+    return createInfoElement(minAndMaxAgePerson, 'li');
+}
 
 
 //TODO click Add person => add unique person to array persons,
--- utils.js
+++ utils.js
@@ -1,12 +1,20 @@
 
 
-function createButtonDel() {
+function createButtonDel(fn) {
     const buttonDel = document.createElement('button');
     buttonDel.appendChild(document.createTextNode('x'));
     buttonDel.classList.add('del');
     buttonDel.onclick = e => {
-        removePersone(e.target.parentElement.id);
         e.target.parentElement.remove();
+        if(typeof (fn) === 'function'){
+            fn();
+        }
     }
     return buttonDel;
+}
+function createInfoElement(content, tag) {
+    const element = document.createElement(tag);
+    const text = document.createTextNode(content);
+    element.appendChild(text);
+    return element;
 }
